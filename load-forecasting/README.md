5572f179a42a79be1a14197bd12dfd13fff9ac16
Alexander Anisimov <alexander.anisimov@dfki.de>
2025-11-12 17:31:57 +0100

    Added query parameters to control the number of samples and whether to include them in the response. Also, dropped some dead code portions in the flask server.

--- load-forecasting/README.md
+++ load-forecasting/README.md
@@ -18,7 +18,7 @@ To this end, clone the code then `cd` to the service directory
 (the directory containing this README) and execute the following command:
 
 ```
-curl http://127.0.0.1:8000/forecast --request POST --header "Content-Type: application/json" --data @./sample_data/example_request.json
+curl --request POST --header "Content-Type: application/json" --data @./sample_data/example_request.json --url-query n_samples=100 --url-query return_samples=False http://127.0.0.1:8000/forecast
 ```
 
 ## Expected input format
@@ -31,15 +31,20 @@ JSON formatted like [example_request.json](./sample_data/example_request.json)
 - Example json file in sample_data
 - 8 days of history
 
-A simple way is to create `pandas.Dataframe` with unnamed datetime index with a frequency of `1h` and one column by the
+A simple way is to create `pandas.DataFrame` with unnamed datetime index with a frequency of `1h` and one column by the
 name of `power`,
 then calling `.to_json()` as is done in [model_provider.py](./src/model_provider.py)
 under `if __name__ == '__main__'`
+
+Additionally, the request can have two query parameters:
+- `n_samples`: `int`, defaults to `100`, determines the number of Monte-Carlo samples to compute the quantiles from
+- `return_samples`: `bool`, defaults to `True`, tells whether to return all samples or the quantiles only
 
 
 ## Output format
 
 JSON file formatted like [example_response.json](./sample_data/example_response.json)
 
-- samples: 100 samples for every horizon from `1h` to `24h` sampled from the learned distribution.
-- quantiles: The quantiles `(0.1, 0.25, 0.5, 0.75, 0.9)`computed from the above samples
+- `samples`: `n` samples for every horizon from `1h` to `24h` sampled from the learned distribution, where `n` is determined by the `n_samples` query parameter.
+    This part is omitted if `return_samples` was set to `False`
+- `quantiles`: The quantiles `(0.1, 0.25, 0.5, 0.75, 0.9)`computed from the above samples
--- load-forecasting/src/flask_server.py
+++ load-forecasting/src/flask_server.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from typing import Optional
-import argparse
 
 import pandas as pd
 from flask import Flask, request, Response
@@ -10,12 +9,30 @@ from src.model_provider import ModelProvider
 from src import project_root
 
 
-expected_content_type = 'application/json'
+expected_content_type = "application/json"
 app = Flask(__name__)
 
 
 def check_header(request):
     return request.content_type == expected_content_type
+
+
+def parse_bool(s: str) -> bool:
+    s = s.strip().lower()
+    if s in ["t", "true"]:
+        return True
+    elif s in ["f", "false"]:
+        return False
+    else:
+        raise ValueError(f"Failed to parse string parameter {s}.")
+
+
+def read_params(request):
+    n_samples = request.args.get("n_samples")
+    return_samples = request.args.get("return_samples")
+    n_samples = 100 if n_samples is None else int(n_samples)
+    return_samples = True if return_samples is None else parse_bool(return_samples)
+    return n_samples, return_samples
 
 
 def read_body(request) -> pd.DataFrame:
@@ -27,7 +44,18 @@ def read_body(request) -> pd.DataFrame:
 @app.route("/forecast", methods=["POST"])
 def energy_forecast():
     if not check_header(request):
-        return Response("Expected content type {} but got {}".format(expected_content_type, request.content_type), 400)
+        return Response(
+            "Expected content type {} but got {}".format(
+                expected_content_type, request.content_type
+            ),
+            400,
+        )
+
+    try:
+        n_samples, return_samples = read_params(request)
+    except Exception as e:
+        print(e)
+        return Response("Failed to parse query parameters", 400)
 
     try:
         history = read_body(request)
@@ -36,7 +64,11 @@ def energy_forecast():
         return Response("Failed to parse request body", 400)
 
     try:
-        forecast = mp.predict(history)
+        forecast = mp.predict(
+            history,
+            n_samples=n_samples,
+            return_samples=return_samples,
+        )
     except Exception as e:
         print(e)
         return Response("Service failed to make a prediction", 500)
@@ -44,67 +76,13 @@ def energy_forecast():
     return Response(forecast, 200)
 
 
-def infer_mounted_model_name(model_path: Path) -> Optional[str]:
-    if not model_path.is_dir():
-        print('The path of the mounted model is not a directory.')
-        return None
-    dir_contents = list(model_path.iterdir())
-    arch_dirs = [d for d in dir_contents if d.suffix == '']
-    if len(dir_contents) < 2 or len(arch_dirs) != 1:
-        print('The path of the mounted model must contain exactly one directory describing the architecture'
-              'and at least one checkpoint containing the weights.')
-        return None
-    model_name = arch_dirs[0].parts[-1]
-    for d in dir_contents:
-        if d.stem != model_name:
-            print('Every checkpoint must belong to the same model as the architecture (denoted by file name).')
-            return None
-    return model_name
-
-
-def parse_optargs() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-s', '--source-model',
-                        help='The model to be used for training or serving',
-                        type=Path, default=project_root.joinpath('models/global'))
-    parser.add_argument('-d', '--dest-model',
-                        help='The path where to store the trained model',
-                        type=Path, required=False)
-    parser.add_argument('-t', '--training-data',
-                        help='A path to the file containing the training data',
-                        type=Path, required=False)
-
-    return parser.parse_args()
-
-
 def create_app():
     global mp
-    mounted_model_path = project_root.joinpath('models/global')
-    mounted_model_name = infer_mounted_model_name(mounted_model_path)
-    print(f'Loading model {mounted_model_name}...')
-    mp = ModelProvider(mounted_model_path.joinpath(mounted_model_name))
+    mounted_model_path = project_root.joinpath("models/global")
+    mounted_model_name = "global"
+    print(f"Loading model {mounted_model_name}...")
+    mp = ModelProvider(
+        mounted_model_path.joinpath(mounted_model_name),
+        # n_samples=args.n_samples,
+    )
     return app
-
-if __name__ == '__main__':
-    args = parse_optargs()
-
-    mounted_model_path = args.source_model
-    mounted_model_name = infer_mounted_model_name(mounted_model_path)
-
-    if mounted_model_path.iterdir() and mounted_model_name is not None:
-        print(f'Loading model {mounted_model_name}...')
-        mp = ModelProvider(mounted_model_path.joinpath(mounted_model_name))
-    else:
-        print(f'No mounted model detected. Falling back to default global model for training or predictions...')
-        mp = ModelProvider()
-
-    if args.dest_model is None and args.training_data is None:
-        print('Launching prediction server...')
-        app.run(debug=True, port=5001, host="0.0.0.0")
-    elif args.dest_model is not None and args.training_data is not None:
-        print('Initializing model training...')
-        mp.fit(training_data_path=args.training_data, fitted_model_path=args.dest_model)
-    else:
-        raise ValueError('Whenever dest-model is provided as console argument, training-data is also expected and vice versa.')
-
